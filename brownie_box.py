52f6c02ec9c1f7d74f15ef2b3b3a18c3b83fc237
Mare <mare.agitatx@gmail.com>
2024-08-28 16:30:35 +0200

    Big update introducing proper CTRW and Bacterium class
    The logic of the previous functions has been changed to operate on
    single values instead of lists (taking advantage of the simulation
    being of the Markov chain's kind so memorylessness is a feature and
    the simulation doesn't need to know the whole past of the system to
    correctly operate).
    Introduced a run_simulation() function that properly simulates a
    continuous time random walk for an instance of the new Bacterium
    class, which may move, die or reproduce.
    As of now, movement is implemented as an update of the space
    coordinates, death is just a simulation loop break and reproduction
    does virtually nothing (the code to simulate more bacteria will come
    later, for now reproduction is a stub)
    
    Also changed the distributions and functions dependent on random
    number's generation since a bug was spotted in their previous form
    where pseudonumbers weren't properly updated, now those functions
    are called passing the default generator of numpy and (says my
    test suite) the functions should be reproducible
    
    The code is yet minimally tested by means of basic unit testing
    
    Also changed in the main code and in tests.py the format of the
    docstrings, that still must be updated since most are empty

--- brownie_box.py
+++ brownie_box.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+import typing
+from enum import Enum
 
 
 def brownian_formula(point_at_t_minus_one, dt, gaussian_term):
-    '''The most simple formula to implement Brownian motion.
+    '''A simple formula to implement Brownian motion.
     Parameters:
     Returns:
     Raises:
@@ -12,120 +14,169 @@ def brownian_formula(point_at_t_minus_one, dt, gaussian_term):
     if dt < 0.:
         raise ValueError('Given a negative time gap to dt.')
 
-    point_at_t = point_at_t_minus_one + np.sqrt(dt) * gaussian_term
+    point_at_t = point_at_t_minus_one + float(np.sqrt(dt)) * gaussian_term
     return point_at_t
 
 
-def gaussian_distribution(mu, sigma, seed=None):
+def gaussian_distribution(mu, sigma, rng=None):
     '''It computes a gaussian distribution's value.
     Parameters:
     Returns:
     '''
-    if seed is None:
-        output = np.random.normal(mu, sigma)
+    if rng is None:
+        gaussian = np.random.normal
     else:
-        random_number_generator = np.random.default_rng(seed)
-        output = random_number_generator.normal(mu, sigma)
+        random_number_generator = rng
+        gaussian = random_number_generator.normal
 
-    return output
+    return gaussian(mu, sigma)
 
-
-def exponential_distribution(beta, seed=None):
+def exponential_distribution(beta, rng=None):
     '''It computes an exponential distribution's value.
     Parameters:
     Returns:
     '''
-    if seed is None:
+    if rng is None:
         output = np.random.exponential(beta)
     else:
-        random_number_generator = np.random.default_rng(seed)
+        random_number_generator = rng
         output = random_number_generator.exponential(beta)
 
     return output
 
 
-def init_time_state(initial_time=None):
-    if initial_time is None:
-        return None
-    return [initial_time]
-
-
-def time_state_updater(time_list, n_points,
-                       time_distribution, *time_parameters):
-    '''It updates the time list by evaluating the distribution given
-    as an input n_points - 1 times.
+def space_state_updater(point_to_update, interval_min, interval_max,
+                        dt, *gaussian_parameters):
+    '''Something something.
     Parameters:
+    Returns:
     '''
-    for i in range(1, n_points):
-        new_time_value = time_list[-1] + time_distribution(*time_parameters)
-        time_list.append(new_time_value)
+    if point_to_update < interval_min or point_to_update > interval_max:
+        raise ValueError('The given point is out of bounds.')
+
+    gaussian_term = gaussian_distribution(*gaussian_parameters)
+    new_point = brownian_formula(point_to_update, dt, gaussian_term)
+
+    if new_point < interval_min:
+        gap = abs(new_point - interval_min)
+        reflected_position = interval_min + gap
+        new_point = reflected_position
+
+    if new_point > interval_max:
+        gap = abs(interval_max - new_point)
+        reflected_position = interval_max - gap
+        new_point = reflected_position
+
+    return new_point
 
 
-def init_space_state(initial_position=None):
-    if initial_position is None:
-        return None
-    return [initial_position]
+class Transition(Enum):
+    DEATH = 'death'
+    REPRODUCTION = 'reproduction'
+    MOVEMENT = 'movement'
 
 
-def space_state_updater(positions, interval_min, interval_max,
-                        times, *gaussian_parameters):
-    '''It updates the positions list by evaluating the brownian formula given
-    before this function.
-    It accepts positions and times both as lists of floats, and 
-    then gaussian parameters as a tuple, so that the user may decide
-    if to give mu, sigma as floats and the rng seed as integer or
-    just mu and sigma without giving the seed.
-    '''
-    if len(times) == 0:
-        return
-    if positions[0] < interval_min or positions[0] > interval_max:
-        raise ValueError('The starting point is out of bounds.')
-
-    previous_time = times[0]
-    for time in times[1:]:
-        dt = time - previous_time
-        gaussian_term = gaussian_distribution(*gaussian_parameters)
-        previous_point = positions[-1]
-        new_point = brownian_formula(previous_point, dt, gaussian_term)
-
-        if new_point < interval_min:
-            gap = abs(new_point - interval_min)
-            reflected_position = interval_min + gap
-            new_point = reflected_position
-
-        if new_point > interval_max:
-            gap = abs(interval_max - new_point)
-            reflected_position = interval_max - gap
-            new_point = reflected_position
-
-        positions.append(new_point)
-        previous_time = time
+class Bacterium(typing.NamedTuple):
+    x: float
+    y: float
+    time_of_observation: float
+    time_of_residency: float
+    transition: Transition
 
 
-def run_simulation(times, x_coordinates, y_coordinates,
-                   mu, sigma, beta, x_0, y_0, t_0, n_points,
-                   x_min, x_max, y_min, y_max):
-    time_state_updater(times, n_points, exponential_distribution, beta)
-    space_state_updater(x_coordinates, x_min, x_max, times, mu, sigma)
-    space_state_updater(y_coordinates, y_min, y_max, times, mu, sigma)
+def death():
+    return 0.005
+
+
+def reproduction():
+    return 0.3
+
+
+def movement():
+    return 1
+
+
+def draw_random_event(transitions_names, rates, rng=None):
+    if len(transitions_names) != len(rates):
+        raise ValueError('Names and rates for the transitions ' +
+                         'must be equal in number.')
+
+    normalized_rates = np.cumsum(rates)
+    normalized_rates /= normalized_rates[-1]
+    if rng is None:
+        p = np.random.random_sample()
+    else:
+        random_number_generator = rng
+        p = random_number_generator.random()
+    idx = np.searchsorted(normalized_rates, p)
+    event = transitions_names[idx]
+    return event
+
+
+def run_simulation(x_0, y_0, x_min, x_max,
+                   y_min, y_max, t_0, time_limit, seed=None):
+    transitions = [death, reproduction, movement]
+    transitions_names = ['death', 'reproduction', 'movement']
+
+    # initializing the variables
+    bacterium_states = []
+    total_time = t_0
+    x, y = x_0, y_0
+    mu, sigma = 0.0, 1.0
+    if seed is not None:
+        rng = np.random.default_rng(seed)
+    else:
+        rng = None
+    gaussian_parameters = (mu, sigma, rng)
+
+    while total_time < time_limit:
+        rates = [f() for f in transitions]
+        total_rate = sum(rates)
+        dt = exponential_distribution(1/total_rate, rng)
+        # drawing the event
+        event = draw_random_event(transitions_names, rates, rng)
+
+        bacterium = Bacterium(x, y, total_time, dt, event)
+        bacterium_states.append(bacterium)
+        total_time += dt
+
+        if event == Transition.REPRODUCTION.value:
+            # same coordinates get stored again, the bacterium
+            # doesn't move
+            continue
+            # here goes the code for spawning another bacterium, WIP
+        elif event == Transition.DEATH.value:
+            # same coordinates get stored again, the bacterium
+            # doesn't move
+            break
+            # here goes the code for removing the bacterium
+            # from the total count, WIP
+        elif event == Transition.MOVEMENT.value:
+            x = space_state_updater(x, x_min, x_max,
+                                    dt, *gaussian_parameters)
+            y = space_state_updater(y, y_min, y_max,
+                                    dt, *gaussian_parameters)
+        else:
+            raise ValueError('Unrecognized transition.')
+    return bacterium_states
 
 
 def plots(times, x_coordinates, y_coordinates, x_min, x_max, y_min, y_max):
-    # plotting x and y over t, to see their time evolution
+
     fig1, ax1 = plt.subplots()
+    ax1.set_title('Time evolution of the coordinates')
     ax1.plot(times, x_coordinates, label='x coordinate', color='orange')
     ax1.plot(times, y_coordinates, label='y coordinate', color='cyan')
     ax1.hlines([x_min, x_max], times[0], times[-1],
-                label='x boundaries', color='orange', linestyle='dashed')
+                label='x boundaries', color='orange', linestyle='dotted')
     ax1.hlines([y_min, y_max],  times[0], times[-1],
-                label='y boundaries', color='cyan', linestyle='dashed')
+                label='y boundaries', color='cyan', linestyle='dotted')
     ax1.legend(loc='best')
     ax1.set_xlabel('Time')
     ax1.set_ylabel('Space')
-    ax1.set_title('Time evolution of the coordinates')
 
-    # plotting y over x, to see the trajectory on the 2d plane
     fig2, ax2 = plt.subplots()
+    ax2.set_title('Trajectory on the xy plane')
     ax2.plot(x_coordinates, y_coordinates, label='trajectory', color='teal')
     ax2.plot(x_0, y_0, label='origin',
              color='red', marker='o', linestyle='None')
@@ -136,28 +187,29 @@ def plots(times, x_coordinates, y_coordinates, x_min, x_max, y_min, y_max):
     ax2.legend(loc='best')
     ax2.set_xlabel("x")
     ax2.set_ylabel("y")
-    ax2.set_title('Trajectory on the xy plane')
+
 
     plt.show()
 
 
 ###############################################################################
 if __name__ == "__main__":
-    # initializing the parameters
-    mu, sigma, beta = 0.0, 1.0, 1.0
+    t_0, time_limit = 0.0, 300.0
+    x_0, y_0 = 0.0, 0.0
     x_min, x_max = 0.0, 20.0
-    y_min, y_max = -12.0, 12.0
-    x_0, y_0, t_0 = 5.0, 2.0, 0.0
-    n_points = 1000
+    y_min, y_max = -10.0, 10.0
+    result = run_simulation(x_0, y_0, x_min, x_max,
+                            y_min, y_max, t_0, time_limit)
+    print('Simulation done.\nSteps of the simulation =', len(result))
 
-    # initializing the states
-    times = init_time_state(t_0)
-    x_coordinates = init_space_state(x_0)
-    y_coordinates = init_space_state(y_0)
+    print('Unpacking the space and time values into lists...')
+    x_coordinates, y_coordinates, times = [], [], []
+    for state in result:
+        x_coordinates.append(state.x)
+        y_coordinates.append(state.y)
+        times.append(state.time_of_observation)
 
-    run_simulation(times, x_coordinates, y_coordinates,
-                   mu, sigma, beta, x_0, y_0, t_0, n_points,
-                   x_min, x_max, y_min, y_max)
+    print('Plotting the lists.')
     plots(times, x_coordinates, y_coordinates, x_min, x_max, y_min, y_max)
 
 
