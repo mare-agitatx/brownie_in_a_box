55c585368b27ae5b12b93cab4a92892be2ba3d56
Mare <mare.agitatx@gmail.com>
2024-08-29 08:45:39 +0200

    Added docstrings and fixed little bugs and typos in tests
    Now every test has a descriptive docstring of the test aim

--- tests.py
+++ tests.py
@@ -3,7 +3,8 @@ import pytest
 
 
 def test_brownian_formula_1():
-    '''
+    '''Testing that the formula gives the expected value with the same values
+    computed in another way.
     '''
     dt = 2.0
     gaussian = 0.5
@@ -15,19 +16,20 @@ def test_brownian_formula_1():
 
 
 def test_brownian_formula_2():
-    '''
+    '''Testing that by giving a zero time interval dt then the point will
+    retain the old position.
     '''
     dt = 0.0
-    gaussian = 0.0
-    previous_point = 0.0
+    gaussian = 2.0
+    previous_point = 1.0
 
-    expected = 0.0
     observed = brownian_formula(previous_point, dt, gaussian)
-    assert observed == pytest.approx(expected)
+    assert observed == pytest.approx(previous_point)
 
 
 def test_brownian_formula_3():
-    '''
+    '''Testing that, by giving a negative time interval dt, brownian_formula()
+    raises a ValueError.
     '''
     with pytest.raises(ValueError):
         dt = -1.0
@@ -35,11 +37,17 @@ def test_brownian_formula_3():
 
 
 def test_gaussian_distribution_1():
-    '''
+    '''Testing reproducibility of the distribution: to do so some values are
+    generated from a seed, the default_rng of numpy and the normal 
+    distribution are then called with that default generator.
+    Then the rng is reset and the distribution to be tested is called with 
+    that rng to check that the generated values are the same of those
+    generated with the standard method.
     '''
     rng = np.random.default_rng(42)
     mu, sigma = 0.0, 1.0
-    expected = [rng.normal(mu, sigma), rng.normal(mu, sigma)]
+    expected = [rng.normal(mu, sigma), rng.normal(mu, sigma),
+                rng.normal(mu, sigma), rng.normal(mu, sigma)]
 
     # the seed must be set again or the next calls
     # to the distribution will proceed with the
@@ -47,14 +55,17 @@ def test_gaussian_distribution_1():
     # while we want to check that the same distribution
     # values are drawn from the same pseudo-random numbers
     rng = np.random.default_rng(42)
-    observed = [gaussian_distribution(0.0, 1.0, rng),
-                gaussian_distribution(0.0, 1.0, rng)]
+    observed = [gaussian_distribution(mu, sigma, rng),
+                gaussian_distribution(mu, sigma, rng),
+                gaussian_distribution(mu, sigma, rng),
+                gaussian_distribution(mu, sigma, rng)]
     for i in range(len(expected)):
         assert observed[i] == pytest.approx(expected[i])
 
 
 def test_gaussian_distribution_2():
-    '''
+    '''Testing that, with a 0.0 standard deviation, the gaussian gives back
+    the mu value as output.
     '''
     mu = 2.0
     observed = gaussian_distribution(mu, 0.0)
@@ -62,11 +73,17 @@ def test_gaussian_distribution_2():
 
 
 def test_exponential_distribution_1():
-    '''
+    '''Testing reproducibility of the distribution: to do so some values are
+    generated from a seed, the default_rng of numpy and the normal 
+    distribution are then called with that default generator.
+    Then the rng is reset and the distribution to be tested is called with 
+    that rng to check that the generated values are the same of those
+    generated with the standard method.
     '''
     rng = np.random.default_rng(69)
     beta = 1.0
-    expected = [rng.exponential(beta), rng.exponential(beta)]
+    expected = [rng.exponential(beta), rng.exponential(beta),
+                rng.exponential(beta), rng.exponential(beta)]
 
     # the seed must be set again or the next calls
     # to the distribution will proceed with the
@@ -75,12 +92,15 @@ def test_exponential_distribution_1():
     # values are drawn from the same pseudo-random numbers
     rng = np.random.default_rng(69)
     observed = [exponential_distribution(beta, rng),
+                exponential_distribution(beta, rng),
+                exponential_distribution(beta, rng),
                 exponential_distribution(beta, rng)]
     assert observed == expected
 
 
 def test_exponential_distribution_2():
-    '''
+    '''Testing that, with a 0.0 beta value (which is like an infinite rate
+    value, since beta = 1/rate), the exponential distribution gives back 0.0.
     '''
     beta = 0.0
 
@@ -89,18 +109,20 @@ def test_exponential_distribution_2():
 
 
 def test_space_state_updater_1():
-    '''
+    '''Testing that, with a zero time interval dt, the space updater leaves the
+    point unchanged.
     '''
     point = 0.1
     initial_point = point
     dt = 0.0
-    space_state_updater(point, 0.0, 1.0, dt, 0.0, 1.0)
+    point = space_state_updater(point, 0.0, 1.0, dt, 0.0, 1.0)
 
     assert point == pytest.approx(initial_point)
 
 
 def test_space_state_updater_2():
-    '''
+    '''Testing that no matter how many times the space updater is called, the
+    final position will always be between the interval boundaries.
     '''
     n_repetitions = 100
     dt = 2.0
@@ -117,7 +139,8 @@ def test_space_state_updater_2():
 
 
 def test_space_state_updater_3():
-    '''
+    '''Testing that, with an origin that is out of bounds, the space updater
+    raises a ValueError.
     '''
     dt = 2.0
     gaussian_parameters = (0.0, 1.0)
@@ -130,7 +153,8 @@ def test_space_state_updater_3():
 
 
 def test_draw_random_event_1():
-    '''
+    '''Testing that the event generated by this function is always one of the
+    given transition names.
     '''
     rates = [0.1, 0.1, 0.1]
     transitions_names = ['event1', 'event2', 'event3']
@@ -139,7 +163,8 @@ def test_draw_random_event_1():
 
 
 def test_draw_random_event_2():
-    '''
+    '''Testing that, by passing a different number of rates and events, the
+    function throws a ValueError.
     '''
     rates = [0.1, 0.1]
     transitions_names = ['event1', 'event2', 'event3', 'event4']
@@ -148,7 +173,8 @@ def test_draw_random_event_2():
 
 
 def test_run_simulation_1():
-    '''
+    '''Testing that, by giving an initial time already at the time limit value,
+    the simulation returns an empty list of states.
     '''
     t_0, time_limit = 30.0, 30.0
     x_0, y_0 = 0.0, 0.0
@@ -161,7 +187,10 @@ def test_run_simulation_1():
 
 
 def test_run_simulation_2():
-    '''
+    '''Testing reproducibility of the simulation: a seed is fixed that will be
+    passed to the numpy default_rng inside the simulation body. The simulation
+    is called two times with the same inputs and stored in two different lists.
+    The test checks that the lists have the same elements.
     '''
     t_0, time_limit = 0.0, 30.0
     x_0, y_0 = 0.0, 0.0
