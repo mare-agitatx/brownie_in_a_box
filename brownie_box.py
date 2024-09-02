2cdf029a012e6c9cdef7f02da80fd95575b4534f
Mare <mare.agitatx@gmail.com>
2024-09-02 12:02:49 +0200

    Big update to brownian_formula and the whole code
    brownian_formula now calls internally the gaussian since to have proper
    brownian motion mu and sigma must have specific values that before had
    to be properly fixed outside this function's scope. The rest of the code
    and the tests have been updated to reflect these changes
    
    Most of the docstrings were reformatted and updated and now every function
    has a proper docstring with info
    
    added one more test to check brownian_formula replicability, fixed some
    minor typos here and there

--- brownie_box.py
+++ brownie_box.py
@@ -5,40 +5,15 @@ import typing
 from enum import Enum
 
 
-def brownian_formula(point_at_t_minus_one, dt, gaussian_term):
-    '''A simple formula to implement Brownian motion.
-    Parameters:
-        point_at_t_minus_one: float, previous position to update
-        dt: float, time interval of the movement
-        gaussian_term: float, gaussian value computed elsewhere; in order
-        to have brownian motion the mean must be 0.0 and the variance
-        deviation must be equal to dt. It can be shown that this is 
-        equivalent to computing a gaussian with mean 0.0 and 
-        standard deviation 1.0 and then multiplying it by sqrt(dt),
-        which this formula does.
-        IT IS IMPORTANT THAT THE GAUSSIAN TERM IS THEN A GAUSSIAN
-        WITH MEAN 0.0 AND STANDARD DEVIATION 1.0
-    Returns:
-        point_at_t: float, the new position computed
-    Raises:
-        ValueError: if dt is negative
-    '''
-    if dt < 0.:
-        raise ValueError('Given a negative time gap to dt.')
-
-    point_at_t = point_at_t_minus_one + float(np.sqrt(dt)) * gaussian_term
-    return point_at_t
-
-
 def gaussian_distribution(mu, sigma, rng=None):
     '''It computes a gaussian distribution's value.
     Parameters:
-        mu: float, the gaussian average parameter
-        sigma: float, the gaussian standard deviation parameter
+        mu: float, the gaussian average parameter.
+        sigma: float, the gaussian standard deviation parameter.
         rng: numpy.random.Generator, the generator of random numbers if passed
-             as input, defaults at None if it isn't passed
+             as input, defaults at None if it isn't passed.
     Returns:
-        output: float, the computed value of the distribution
+        output: float, the computed value of the distribution.
     '''
     if rng is None:
         output = np.random.normal(mu, sigma)
@@ -52,11 +27,11 @@ def exponential_distribution(beta, rng=None):
     '''It computes an exponential distribution's value.
     Parameters:
         beta: float, the beta value of the exponential, equal to
-              1/rate if the distribution is thought as function of rate
+              1/rate if the distribution is thought as function of rate.
         rng: numpy.random.Generator, the generator of random numbers if passed
-             as input, defaults at None if it isn't passed
+             as input, defaults at None if it isn't passed.
     Returns:
-        output: float, the computed value of the distribution
+        output: float, the computed value of the distribution.
     '''
     if rng is None:
         output = np.random.exponential(beta)
@@ -67,24 +42,47 @@ def exponential_distribution(beta, rng=None):
     return output
 
 
-def space_state_updater(point_to_update, interval_min, interval_max,
-                        dt, *gaussian_parameters):
-    '''Updater function to compute the next position for the bacterium in the
-       simulation
+def brownian_formula(point_at_t_minus_one, dt, rng=None):
+    '''A simple formula to implement Brownian motion.
     Parameters:
-        point_to_update:
-        interval_min:
-        interval_max:
-        dt:
-        *gaussian_parameters:
+        point_at_t_minus_one: float, previous position to update.
+        dt: float, time interval of the movement.
+        rng: numpy.random.Generator, the generator of random numbers if passed
+             as input, defaults at None if it isn't passed.
     Returns:
-        new_point:
+        point_at_t: float, the new position computed.
+    Raises:
+        ValueError: if dt is negative.
+    '''
+    if dt < 0.:
+        raise ValueError('Given a negative time gap to dt.')
+
+    gaussian_term = gaussian_distribution(0.0, 1.0, rng)
+    point_at_t = point_at_t_minus_one + float(np.sqrt(dt)) * gaussian_term
+    return point_at_t
+
+
+def space_state_updater(point_to_update, interval_min, interval_max,
+                        dt, rng=None):
+    '''Updater function to compute the next position for the bacterium in the
+       simulation.
+    Parameters:
+        point_to_update: float, the initial point to be computed.
+        interval_min: float, the minimum value that new_point can have.
+        interval_max: float, the maximum value that new_point can have.
+        dt: float, time interval of the movement.
+        rng: numpy.random.Generator, the generator of random numbers if passed
+             as input, defaults at None if it isn't passed.
+    Returns:
+        new_point: float, the new position computed.
+    Raises:
+        ValueError: if point_to_update is less than interval_min
+                    or more than interval_max.
     '''
     if point_to_update < interval_min or point_to_update > interval_max:
         raise ValueError('The given point is out of bounds.')
 
-    gaussian_term = gaussian_distribution(*gaussian_parameters)
-    new_point = brownian_formula(point_to_update, dt, gaussian_term)
+    new_point = brownian_formula(point_to_update, dt, rng)
 
     if new_point < interval_min:
         gap = abs(new_point - interval_min)
@@ -134,37 +132,60 @@ def movement():
     return 1
 
 
-def draw_random_event(transitions_names, rates, rng=None):
-    '''
+def draw_random_event(transition_names, transition_rates, rng=None):
+    '''It draws randomly an event string given names and rates.
     Parameters:
+        transition_names = iterable of strings, the events to draw from.
+        transition_rates = iterable of floats, the rates related to
+                           probabilities of those events happening,
+                           not normalized.
+        rng = numpy.random.Generator, the generator of random numbers if passed
+             as input, defaults at None if it isn't passed.
     Returns:
+        event: string, the event drawn randomly.
     Raises:
+        ValueError: if the iterables of transition_names and transition_rates
+                    have different lengths.
     '''
-    if len(transitions_names) != len(rates):
+    if len(transition_names) != len(transition_rates):
         raise ValueError('Names and rates for the transitions ' +
                          'must be equal in number.')
 
-    normalized_rates = np.cumsum(rates)
+    # normalizing the rates
+    normalized_rates = np.cumsum(transition_rates)
     normalized_rates /= normalized_rates[-1]
     if rng is None:
-        p = np.random.random_sample()
+        probability = np.random.random_sample()
     else:
         random_number_generator = rng
-        p = random_number_generator.random()
-    idx = np.searchsorted(normalized_rates, p)
-    event = transitions_names[idx]
+        probability = random_number_generator.random()
+    # drawing randomly the event
+    index = np.searchsorted(normalized_rates, probability)
+    event = transition_names[index]
     return event
 
 
 def run_simulation(x_0, y_0, x_min, x_max,
                    y_min, y_max, t_0, time_limit, seed=None):
-    '''
+    '''The function that runs the main simulation of the bacterium.
     Parameters:
+        x_0, y_0: floats, coordinates of the origin
+        x_min, x_max: floats, interval limits for the x coordinate
+        y_min, y_max: floats, interval limits for the y coordinate
+        t_0, time_limit: floats, first instant and last instant
+                         of the simulation
+        seed: integer if given, will be passed to the default number
+              generator of numpy. If not passed will default to None
+              and the rng won't be called
     Returns:
+        bacterium_states: list of lists of Bacterium instances, the
+        data generated by the simulation
     Raises:
+        ValueError: if an event is drawn randomly that isn't found in
+                    transition_names
     '''
     transitions = [death, reproduction, movement]
-    transitions_names = ['death', 'reproduction', 'movement']
+    transition_names = ['death', 'reproduction', 'movement']
 
     # initializing the variables
     bacterium_states = []
@@ -175,14 +196,13 @@ def run_simulation(x_0, y_0, x_min, x_max,
         rng = np.random.default_rng(seed)
     else:
         rng = None
-    gaussian_parameters = (mu, sigma, rng)
 
     while total_time < time_limit:
         rates = [f() for f in transitions]
         total_rate = sum(rates)
         dt = exponential_distribution(1/total_rate, rng)
         # drawing the event
-        event = draw_random_event(transitions_names, rates, rng)
+        event = draw_random_event(transition_names, rates, rng)
 
         bacterium = Bacterium(x, y, total_time, dt, event)
         bacterium_states.append(bacterium)
@@ -201,17 +221,22 @@ def run_simulation(x_0, y_0, x_min, x_max,
             # from the total count, WIP
         elif event == Transition.MOVEMENT.value:
             x = space_state_updater(x, x_min, x_max,
-                                    dt, *gaussian_parameters)
+                                    dt, rng)
             y = space_state_updater(y, y_min, y_max,
-                                    dt, *gaussian_parameters)
+                                    dt, rng)
         else:
             raise ValueError('Unrecognized transition.')
     return bacterium_states
 
 
 def plots(times, x_coordinates, y_coordinates, x_min, x_max, y_min, y_max):
-    '''
+    '''Plots the given lists as time evolutions and a trajectory.
     Parameters:
+        times: list of floats, containing the times of observation
+        x_coordinates, y_coordinates: lists of floats, containing
+                                      the various positions
+        x_min, x_max: floats, interval limits for the x coordinate
+        y_min, y_max: floats, interval limits for the y coordinate
     '''
 
     fig1, ax1 = plt.subplots()
@@ -248,8 +273,9 @@ if __name__ == "__main__":
     x_0, y_0 = 0.0, 0.0
     x_min, x_max = 0.0, 20.0
     y_min, y_max = -10.0, 10.0
+    seed = 500
     result = run_simulation(x_0, y_0, x_min, x_max,
-                            y_min, y_max, t_0, time_limit)
+                            y_min, y_max, t_0, time_limit, seed)
     print('Simulation done.\nSteps of the simulation =', len(result))
 
     print('Unpacking the space and time values into lists...')
--- tests.py
+++ tests.py
@@ -2,54 +2,20 @@ from brownie_box import *
 import pytest
 
 
-def test_brownian_formula_1():
-    '''Testing that the formula gives the expected value with the same values
-    computed in another way.
-    '''
-    dt = 2.0
-    gaussian = 0.5
-    previous_point = 3.0
-
-    expected = 3.0 + 0.5 * 1.41421356
-    observed = brownian_formula(previous_point, dt, gaussian)
-    assert observed == pytest.approx(expected)
-
-
-def test_brownian_formula_2():
-    '''Testing that by giving a zero time interval dt then the point will
-    retain the old position.
-    '''
-    dt = 0.0
-    gaussian = 2.0
-    previous_point = 1.0
-
-    observed = brownian_formula(previous_point, dt, gaussian)
-    assert observed == pytest.approx(previous_point)
-
-
-def test_brownian_formula_3():
-    '''Testing that, by giving a negative time interval dt, brownian_formula()
-    raises a ValueError.
-    '''
-    with pytest.raises(ValueError):
-        dt = -1.0
-        brownian_formula(1.0, dt, 1.0)
-
-
 def test_gaussian_distribution_1():
     '''Testing reproducibility of the distribution: to do so some values are
-    generated from a seed, the default_rng of numpy and the normal 
-    distribution are then called with that default generator.
-    Then the rng is reset and the distribution to be tested is called with 
-    that rng to check that the generated values are the same of those
-    generated with the standard method.
+       generated from a seed, the default_rng of numpy and the normal
+       distribution are then called with that default generator.
+       Then the rng is reset and the distribution to be tested is called with
+       that rng to check that the generated values are the same of those
+       generated with the standard method.
     '''
     rng = np.random.default_rng(42)
     mu, sigma = 0.0, 1.0
     expected = [rng.normal(mu, sigma), rng.normal(mu, sigma),
                 rng.normal(mu, sigma), rng.normal(mu, sigma)]
 
-    # the seed must be set again or the next calls
+    # the rng must be set again or the next calls
     # to the distribution will proceed with the
     # following pseudo-random numbers in the sequence,
     # while we want to check that the same distribution
@@ -65,7 +31,7 @@ def test_gaussian_distribution_1():
 
 def test_gaussian_distribution_2():
     '''Testing that, with a 0.0 standard deviation, the gaussian gives back
-    the mu value as output.
+       the mu value as output.
     '''
     mu = 2.0
     observed = gaussian_distribution(mu, 0.0)
@@ -74,18 +40,18 @@ def test_gaussian_distribution_2():
 
 def test_exponential_distribution_1():
     '''Testing reproducibility of the distribution: to do so some values are
-    generated from a seed, the default_rng of numpy and the normal 
-    distribution are then called with that default generator.
-    Then the rng is reset and the distribution to be tested is called with 
-    that rng to check that the generated values are the same of those
-    generated with the standard method.
+       generated from a seed, the default_rng of numpy and the normal 
+       distribution are then called with that default generator.
+       Then the rng is reset and the distribution to be tested is called with 
+       that rng to check that the generated values are the same of those
+       generated with the standard method.
     '''
     rng = np.random.default_rng(69)
     beta = 1.0
     expected = [rng.exponential(beta), rng.exponential(beta),
                 rng.exponential(beta), rng.exponential(beta)]
 
-    # the seed must be set again or the next calls
+    # the rng must be set again or the next calls
     # to the distribution will proceed with the
     # following pseudo-random numbers in the sequence,
     # while we want to check that the same distribution
@@ -100,7 +66,7 @@ def test_exponential_distribution_1():
 
 def test_exponential_distribution_2():
     '''Testing that, with a 0.0 beta value (which is like an infinite rate
-    value, since beta = 1/rate), the exponential distribution gives back 0.0.
+       value, since beta = 1/rate), the exponential distribution gives back 0.0.
     '''
     beta = 0.0
 
@@ -108,21 +74,92 @@ def test_exponential_distribution_2():
     assert observed == pytest.approx(0.0)
 
 
+def test_brownian_formula_1():
+    '''Testing that the formula gives the expected value with the same values
+       computed in another way.
+    '''
+    dt = 2.0
+    seed = 20
+    rng = np.random.default_rng(seed)
+    gaussian = gaussian_distribution(0.0, 1.0, rng)
+    previous_point = 3.0
+    expected = 3.0 + gaussian * 1.41421356
+
+    # the rng must be set again or the next call
+    # to the distribution will proceed with the
+    # following pseudo-random numbers in the sequence,
+    # while we want to check the formula with the
+    # same generated value
+    rng = np.random.default_rng(seed)
+    observed = brownian_formula(previous_point, dt, rng)
+    assert observed == pytest.approx(expected)
+
+
+def test_brownian_formula_2():
+    '''Testing that by giving a zero time interval dt then the point will
+       retain the old position.
+    '''
+    dt = 0.0
+    previous_point = 1.0
+
+    observed = brownian_formula(previous_point, dt)
+    assert observed == pytest.approx(previous_point)
+
+
+def test_brownian_formula_3():
+    '''Testing that, by giving a negative time interval dt, brownian_formula()
+       raises a ValueError.
+    '''
+    with pytest.raises(ValueError):
+        dt = -1.0
+        brownian_formula(1.0, dt)
+
+
+def test_brownian_formula_4():
+    '''Testing that the function has proper replicability by calling it
+       multiple times and then doing it again with the rng reset and
+       comparing the results stored in different lists.
+    '''
+    n_repetitions = 100
+    list1, list2 = [], []
+    seed = 900
+    dt = 1.0
+
+    rng = np.random.default_rng(seed)
+    observed = 0.0
+    for index in range(n_repetitions):
+        observed = brownian_formula(observed, dt,  rng)
+        list1.append(observed)
+
+    # the rng must be set again or the next call
+    # to the distribution will proceed with the
+    # following pseudo-random numbers in the sequence,
+    # while we want to check the formula with the
+    # same generated value
+    rng = np.random.default_rng(seed)
+    # observed must also be reset
+    observed = 0.0
+    for index in range(n_repetitions):
+        observed = brownian_formula(observed, dt, rng)
+        list2.append(observed)
+
+    assert list1 == list2
+
+
 def test_space_state_updater_1():
     '''Testing that, with a zero time interval dt, the space updater leaves the
-    point unchanged.
+       point unchanged.
     '''
     point = 0.1
     initial_point = point
-    dt = 0.0
-    point = space_state_updater(point, 0.0, 1.0, dt, 0.0, 1.0)
+    point = space_state_updater(point, 0.0, 1.0, 0.0)
 
     assert point == pytest.approx(initial_point)
 
 
 def test_space_state_updater_2():
     '''Testing that no matter how many times the space updater is called, the
-    final position will always be between the interval boundaries.
+       final position will always be between the interval boundaries.
     '''
     n_repetitions = 100
     dt = 2.0
@@ -132,7 +169,7 @@ def test_space_state_updater_2():
     observed = 0.5
     for i in range(n_repetitions):
         observed = space_state_updater(observed, s_min, s_max,
-                                       dt, *gaussian_parameters)
+                                       dt)
 
     assert observed >= s_min
     assert observed <= s_max
@@ -140,7 +177,7 @@ def test_space_state_updater_2():
 
 def test_space_state_updater_3():
     '''Testing that, with an origin that is out of bounds, the space updater
-    raises a ValueError.
+       raises a ValueError.
     '''
     dt = 2.0
     gaussian_parameters = (0.0, 1.0)
@@ -149,12 +186,12 @@ def test_space_state_updater_3():
     with pytest.raises(ValueError):
         out_of_bounds_origin = 500.0
         observed = space_state_updater(out_of_bounds_origin,
-                                       s_min, s_max, dt, *gaussian_parameters)
+                                       s_min, s_max, dt)
 
 
 def test_draw_random_event_1():
     '''Testing that the event generated by this function is always one of the
-    given transition names.
+       given transition names.
     '''
     rates = [0.1, 0.1, 0.1]
     transitions_names = ['event1', 'event2', 'event3']
@@ -164,7 +201,7 @@ def test_draw_random_event_1():
 
 def test_draw_random_event_2():
     '''Testing that, by passing a different number of rates and events, the
-    function throws a ValueError.
+       function throws a ValueError.
     '''
     rates = [0.1, 0.1]
     transitions_names = ['event1', 'event2', 'event3', 'event4']
@@ -174,7 +211,7 @@ def test_draw_random_event_2():
 
 def test_run_simulation_1():
     '''Testing that, by giving an initial time already at the time limit value,
-    the simulation returns an empty list of states.
+       the simulation returns an empty list of states.
     '''
     t_0, time_limit = 30.0, 30.0
     x_0, y_0 = 0.0, 0.0
@@ -188,9 +225,9 @@ def test_run_simulation_1():
 
 def test_run_simulation_2():
     '''Testing reproducibility of the simulation: a seed is fixed that will be
-    passed to the numpy default_rng inside the simulation body. The simulation
-    is called two times with the same inputs and stored in two different lists.
-    The test checks that the lists have the same elements.
+       passed to the numpy default_rng inside the simulation body. The
+       simulation is called two times with the same inputs and stored in two
+       different lists. The test checks that the lists have the same elements.
     '''
     t_0, time_limit = 0.0, 30.0
     x_0, y_0 = 0.0, 0.0
