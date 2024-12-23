691e25f0b48e86dd35c401fa9dc2f2304f7d6528
Mare <mare.agitatx@gmail.com>
2024-12-23 18:02:02 +0100

    Added angular plots and log scale to chi squared
    Now six graphs are plotted: the new ones are the distributions of theta,
    the polar angle that goes like sin(theta)/2, and phi, the azimutal
    angle that goes like a uniform distribution
    
    The chi squared plot for r^2 now is in log scale to better focus on
    the right tail

--- br_distributions.py
+++ br_distributions.py
@@ -9,14 +9,14 @@ import matplotlib.pyplot as plt
 def multiple_histogram_plots(coords_dict_lists, figure_title, x_0, y_0, z_0):
     '''
     Routine to generate the plots of the distributions as histograms and
-    then some gaussians.
+    then some theoretical distributions.
     Parameters:
         coords_dict_lists: dictionary, the entries containing the values for
                            the variables
         figure_title: string, title given to the figure.
         x_0, y_0 z_0: floats, coordinates of the origin.
     '''
-    fig, axs = plt.subplots(2, 2)
+    fig, axs = plt.subplots(2, 3)
     coords_list = list(coords_dict_lists)
     sigmas = []
     for i, ax in enumerate(fig.axes):
@@ -28,7 +28,10 @@ def multiple_histogram_plots(coords_dict_lists, figure_title, x_0, y_0, z_0):
         # evaluating distributions from
         # empirical parameters as references
         t = np.linspace(min(variable_values), max(variable_values), 10000)
-        if 'radial' not in variable_name:
+        cartesian_check = 'x' in variable_name
+        cartesian_check = cartesian_check or ('y' in variable_name)
+        cartesian_check = cartesian_check or ('z' in variable_name)
+        if cartesian_check is True:
             mu = np.mean(variable_values)
             sigma = np.std(variable_values, ddof=1)
             norm_pdf = st.norm.pdf(t, mu, sigma)
@@ -36,19 +39,39 @@ def multiple_histogram_plots(coords_dict_lists, figure_title, x_0, y_0, z_0):
             sigmas.append(sigma)
             var_title = variable_name + ' (gaussian)'
             param_str = f'mu = {mu:.2f}' + '\n' + f'sigma = {sigma:.2f}'
-        else:
+        elif 'radial' in variable_name:
             dof = 3 # x, y and z, degrees of freedom
             loc = 0 # required from scipy.stats to plot
             scale = np.mean(sigmas)**2 # or chi2 will not be properly scaled
             chi2_pdf = st.chi2.pdf(t, dof, loc, scale)
+            ax.set_yscale('log')
             ax.plot(t, chi2_pdf)
             var_title = variable_name + ' (chi squared)'
             param_str = 'degrees of freedom = ' + str(dof) + '\n'
             param_str += f'scale factor = ({np.mean(sigmas):.2f})^2'
-        
+        elif 'theta' in variable_name:
+            # scipi.stats has no in-built distributions
+            # for the sine; the expected distribution
+            # for theta is sin(theta) and it must be 
+            # normalized by a 1/2 factor, the area under
+            # the sine curve from 0 to pi, which is
+            # the expected interval for theta
+            sine_pdf = np.sin(t) / 2
+            ax.plot(t, sine_pdf)
+            var_title = variable_name + ' (sine)'
+            param_str = None
+        elif 'phi' in variable_name:
+            loc = np.pi * (-1) # starts at -pi
+            scale = np.pi * 2 # spans from -pi to +pi, the gap is 2*pi
+            unif_pdf = st.uniform.pdf(t, loc, scale)
+            ax.plot(t, unif_pdf)
+            var_title = variable_name + ' (uniform)'
+            param_str = None
+
         ax.hist(variable_values, bins='auto', density=True)
-        ax.text(0.95, 0.95, param_str, transform=ax.transAxes, fontsize=10,
-                verticalalignment='top', horizontalalignment='right')
+        if param_str is not None:
+            ax.text(0.95, 0.95, param_str, transform=ax.transAxes, fontsize=10,
+                    verticalalignment='top', horizontalalignment='right')
         ax.set_title(var_title)
         
     fig.suptitle(figure_title)
@@ -58,8 +81,8 @@ def multiple_histogram_plots(coords_dict_lists, figure_title, x_0, y_0, z_0):
 def multiple_distributions_routine(bacteria_list, time_of_interest,
                                    figure_title, x_0, y_0, z_0):
     '''
-    Routine to evaluate four distributions for x, y, z and the radii,
-    by means of histograms, three gaussians and a chi squared bells.
+    Routine to evaluate six distributions for x, y, z, the radii, theta and phi
+    by means of histograms and theoretical expected curves.
     Parameters:
         bacteria_list: list of dictionaries, representing the bacteria.
         time_of_interest: float, the time at which the distributions are
@@ -98,6 +121,9 @@ def multiple_distributions_routine(bacteria_list, time_of_interest,
         xyzs.append(some_tuple)
 
     coords_dict_lists['radial^2'] = bra.radial_distribution(xyzs, x_0, y_0, z_0)
+    thetas, phis = bra.angular_distribution(xyzs, x_0, y_0, z_0)
+    coords_dict_lists['theta'] = thetas
+    coords_dict_lists['phi'] = phis
     multiple_histogram_plots(coords_dict_lists, figure_title, x_0, y_0, z_0)
 
 
